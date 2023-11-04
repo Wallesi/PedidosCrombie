a015545a871155a14007d940aeaa88470177992c
wallesi <waldemargalizzi98@gmail.com>
2023-11-04 11:48:04 -0300

    other column added

--- src/app/User/delivery/historial/page.tsx
+++ src/app/User/delivery/historial/page.tsx
@@ -9,7 +9,7 @@ export default function () {
                 <div className="flex flex-col items-center p-4 md:flex-row md:items-center md:justify-between border border-slate-300 rounded-xl gap-2">
                     <div className="text-center md:text-left">
                         <div>
-                            <h1 className="text-base font-bold text-center">Emisor</h1>
+                            <h1 className="text-base font-bold text-center">Restaurante</h1>
                             <p className="">
                                 <span className="inline">San Martin</span>
                                 <span className="mx-1 inline">1948</span>
@@ -17,6 +17,31 @@ export default function () {
                             <p>
                                 <span className="inline">Restaurante</span>
                                 <span className="mx-1 inline">La City Sport</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="text-center md:text-left">
+                        <div>
+                            <h1 className="text-base font-bold text-center">Repartidor</h1>
+                            <p className="">
+                                <span className="inline">Bicicleta</span>
+                            </p>
+                            <p>
+                                <span className="inline">Nicolas</span>
+                                <span className="mx-1 inline">Muller</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="text-center md:text-right">
+                        <div>
+                            <h1 className="text-base font-bold text-center">Cliente</h1>
+                            <p className="">
+                                <span className="inline">Bv.Galvez</span>
+                                <span className="mx-1 inline">1948</span>
+                            </p>
+                            <p>
+                                <span className="inline">Leandro</span>
+                                <span className="mx-1 inline">Perez</span>
                             </p>
                         </div>
                     </div>
@@ -31,19 +56,7 @@ export default function () {
                             </p>
                         </div>
                     </div>
-                    <div className="text-center md:text-right">
-                        <div>
-                            <h1 className="text-base font-bold text-center">Receptor</h1>
-                            <p className="">
-                                <span className="inline">Bv.Galvez</span>
-                                <span className="mx-1 inline">1948</span>
-                            </p>
-                            <p>
-                                <span className="inline">Leandro</span>
-                                <span className="mx-1 inline">Perez</span>
-                            </p>
-                        </div>
-                    </div>
+                    
 
                     <div className="flex gap-2">
                         <button className=" bg-green-400 p-3 border rounded-2xl hover:bg-green-300">
@@ -54,11 +67,12 @@ export default function () {
                         </button>
                     </div>
                 </div>
+            </div>
 
-                <div className="mt-5 flex flex-col items-center p-4 md:flex-row md:items-center md:justify-between border border-slate-300 rounded-xl">
+            <div className="flex flex-col items-center p-4 md:flex-row md:items-center md:justify-between border border-slate-300 rounded-xl">
                     <div className="text-center md:text-left">
                         <div>
-                            <h1 className="text-base font-bold text-center">Emisor</h1>
+                            <h1 className="text-base font-bold text-center">Restaurante</h1>
                             <p className="">
                                 <span className="inline">San Martin</span>
                                 <span className="mx-1 inline">1948</span>
@@ -66,6 +80,31 @@ export default function () {
                             <p>
                                 <span className="inline">Restaurante</span>
                                 <span className="mx-1 inline">La City Sport</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="text-center md:text-left">
+                        <div>
+                            <h1 className="text-base font-bold text-center">Repartidor</h1>
+                            <p className="">
+                                <span className="inline">Bicicleta</span>
+                            </p>
+                            <p>
+                                <span className="inline">Nicolas</span>
+                                <span className="mx-1 inline">Muller</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="text-center md:text-right">
+                        <div>
+                            <h1 className="text-base font-bold text-center">Cliente</h1>
+                            <p className="">
+                                <span className="inline">Bv.Galvez</span>
+                                <span className="mx-1 inline">1948</span>
+                            </p>
+                            <p>
+                                <span className="inline">Leandro</span>
+                                <span className="mx-1 inline">Perez</span>
                             </p>
                         </div>
                     </div>
@@ -80,32 +119,17 @@ export default function () {
                             </p>
                         </div>
                     </div>
-                    <div className="text-center md:text-right">
-                        <div>
-                            <h1 className="text-base font-bold text-center">Receptor</h1>
-                            <p className="">
-                                <span className="inline">Bv.Galvez</span>
-                                <span className="mx-1 inline">1948</span>
-                            </p>
-                            <p>
-                                <span className="inline">Leandro</span>
-                                <span className="mx-1 inline">Perez</span>
-                            </p>
-                        </div>
-                    </div>
+                    
 
                     <div className="flex gap-2">
                         <button className=" bg-green-400 p-3 border rounded-2xl hover:bg-green-300">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
                             Info
                         </button>
                     </div>
                 </div>
-
-            </div>
-
 
 
         </div>
